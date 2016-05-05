fbe4862cf7c48048dfd875214b342b4e38c41a7c
ufeanei <akaranjoh@yahoo.com>
2016-05-05 15:53:01 +0200

    reset password functionality completed

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -15,17 +15,52 @@ else
   flash.now[:danger] = "Email address not found"
   render :new
 end
-
 end
 
 def edit
+  @user = User.find_by(email: params[:email])  
 end
 
 
 def update
-end
+  @user = User.find_by(email: params[:email])
+  if params[:user][:password].empty?
+    @user.errors.add(:password, "can't be empty")
+    render 'edit'
+  elsif @user.update_attributes(user_params)
+    log_in @user
+    flash[:success] = "Password has been reset."
+    redirect_to @user
+  else
+    render 'edit'
+  end
+end 
 
+private
 
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
+  # Before filters
+  def get_user
+    @user = User.find_by(email: params[:email])
+  end
+
+  # Confirms a valid user.
+  def valid_user
+    unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
+      redirect_to jobs_path
+    end
+  end
+
+  # Checks expiration of reset token.
+  def check_expiration
+    if @user.password_reset_expired?
+      flash[:danger] = "Password reset has expired."
+      redirect_to new_password_reset_url
+    end
+  end
 
   
 end
