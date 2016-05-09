056730ca04cedadf1af564d667c3bb4d7812240f
ufeanei <akaranjoh@yahoo.com>
2016-05-09 02:01:22 +0200

    refactored jobs controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,36 +5,26 @@ class JobsController < ApplicationController
   before_action :get_all_fylke, only: [:index, :new, :edit]
 
   def index
-    #@fylkes = Fylke.all
-
     @all_jobs= Job.all
     if requested_jobs
       @jobs=Job.where(fylke_id: requested_jobs).order(created_at: :desc).paginate(page: params[:page], per_page: 5)
     else
       @jobs = @all_jobs.order(created_at: :desc).paginate(page: params[:page], per_page: 5)
     end
-  
-   # @jobs = (requested_jobs ? Job.where(fylke_id: requested_jobs) : @all_jobs).order(created_at: :desc).paginate(page: params[:page], per_page: 5)
     respond_to do |format|
-      format.html
-    
+      format.html  
       format.js 
-    end
-    
+    end    
   end 
 
-  
-
   def new
-    #@fylkes = Fylke.all 
+    
     @job = Job.new
   end
 
   def create    
     @job = Job.new(job_params)
     @job.user = current_user
-
-   
     if @job.save
       flash[:succes] = 'your job has been created'
       redirect_to jobs_path
@@ -44,12 +34,10 @@ class JobsController < ApplicationController
   end
 
   def edit
-    #@fylkes = Fylke.all
-    #@job = Job.find_by(id: params[:id]) 
+    
   end
 
   def update
-    #@job = Job.find_by(id: params[:id])
      if @job.update(job_params)
       flash[:success] = "your job has been update"
       redirect_to dashboard_path
@@ -59,7 +47,6 @@ class JobsController < ApplicationController
   end
 
   def show   
-    #@job = Job.find_by(id: params[:id])
     @same_location_jobs = Job.where(fylke_id: @job.fylke_id).limit(6).order(created_at: :desc)
   end
 
