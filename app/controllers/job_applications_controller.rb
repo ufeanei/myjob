a3b2dcb28915a6da322b6512197b2da34e3934fe
ufeanei <akaranjoh@yahoo.com>
2016-05-11 21:30:58 +0200

    job application draft completed

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -1,25 +1,37 @@
 class JobApplicationsController < ApplicationController
-	before_action :require_user
-	def create
+  before_action :require_user
+  def create
      job_application = JobApplication.new(jobparams)
      if job_application.save
-     	flash[:success] = "application succesful. Wait for job owner to invite you"
-     	redirect_to :back
+      flash[:success] = "application succesful. Wait for job owner to invite you"
+      redirect_to :back
      end
 
      def destroy
-     	#delete an application
+      JobApplication.find_by(id: 1).delete
+      job_application = JobApplication.find_by(id: 1)
+      if 
+      #delete an application
      end
 
      def award
-     	#award an application to a user
+      #award an application to a user
+      job_application = JobApplication.find_by(id: 1)
+      if job_application.update_attribute(awarded: true)
+        flash[:success] = "Invitation succesful. Helper has been notified by email"
+        redirect_to :back
+        #send notification email to user as a background job
+      end
      end
 
      def cancel_award
-     	#cancel an already awarded application
+      #cancel an already awarded application
+      job_application = JobApplication.find_by(id: 1)
+      if job_application.update_attribute(awarded: false)
+        flash[:success] = "Invitation succesful canceled"
+        redirect_to :back
+        #send notification email to user as a background job
+      end
      end
-
-
-
-	end
+  end
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,21 +1,20 @@
 class Job < ActiveRecord::Base
-	attr_accessor :terms_of_service
-validates :title,  presence: true, length: {maximum: 90}
-validates :description, presence: true
-validates :payment, numericality: true 
-validates :street_addr, presence: true
-validates :destination_addr, presence: true
-validates :contact_number, presence: true
-validates :kommune_id, presence: true
-validates :fylke_id, presence: true
-validates :terms_of_service, acceptance: {accept: "1"}
+  attr_accessor :terms_of_service
+  
+  validates :title,  presence: true, length: {maximum: 90}
+  validates :description, presence: true
+  validates :payment, numericality: true 
+  validates :street_addr, presence: true
+  validates :destination_addr, presence: true
+  validates :contact_number, presence: true
+  validates :kommune_id, presence: true
+  validates :fylke_id, presence: true
+  validates :terms_of_service, acceptance: {accept: "1"}
 
-
-
-belongs_to :fylke
-belongs_to :kommune
-belongs_to :user
-has_many :job_applications
+  belongs_to :fylke
+  belongs_to :kommune
+  belongs_to :user
+  has_many :job_applications
 
 end
 
