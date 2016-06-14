ac77c364795b78229727cb749fc9ba40c3884776
ufeanei <akaranjoh@yahoo.com>
2016-06-15 01:04:33 +0200

    added sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,13 +49,13 @@ Porter::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: "smtp.sendgrid.net",
     port: 587,
     domain: "heroku.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: ENV["SENDGRID_USERNAME"],
+    password: ENV["SENDGRID_PASSWORD"]
   }
 
   config.action_mailer.default_url_options = { :host => 'obscure-beyond-36942.herokuapp.com' }
