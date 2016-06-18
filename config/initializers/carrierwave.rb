aa4dbf2748325055c32f14690b0442d4bd4eeb70
ufeanei <akaranjoh@yahoo.com>
2016-06-18 23:43:55 +0200

    set aws env variables on heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,12 +5,12 @@ if Rails.env.production?
 
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV["ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["SECRET_ACCESS_KEY"],
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
       region: 'eu-west-1'
     }
 
 
-    config.fog_directory = ENV['S3-BUCKET']
+    config.fog_directory = ENV['S3-BUCKET_NAME']
   end
 end
