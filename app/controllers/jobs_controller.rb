5d4beebda3c499c3f9929d60678a3f63e030a60d
ufeanei <akaranjoh@yahoo.com>
2016-08-03 05:38:33 +0200

    Corrected copyright in footer

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -92,7 +92,6 @@ class JobsController < ApplicationController
 
  private
 
-
   def job_params
     params.require(:job).permit(:title,:description, :payment, :category_id, :street_addr, :destination_addr, :contact_number, 
       :kommune_id, :fylke_id, :terms_of_service, :image, :image_cache)
